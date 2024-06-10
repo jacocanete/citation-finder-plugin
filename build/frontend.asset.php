2cf3d02bd8904e1ba8f1967994f6a7a2159b2b4d
jacocanete <jacogagarincanete@gmail.com>
2024-06-10 22:57:10 +0800

    chore: Update frontend styles for .wp-block-post-title and remove unnecessary CSS files

--- build/frontend.asset.php
+++ build/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components'), 'version' => '9ce10d2d7359966aae68');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components'), 'version' => '0e9cccf6ab682dabb875');
